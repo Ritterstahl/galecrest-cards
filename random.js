a797c92106377b01c96635c614986af1ae1d3e86
Ritterstahl <72339963+Ritterstahl@users.noreply.github.com>
2022-06-14 16:48:33 +0100

    Update random.js

--- random.js
+++ random.js
@@ -1,2 +1,9 @@
-var x = Math.floor((Math.random() * 40) + 1);
-document.getElementById("demo").innerHTML = x;
+//var x = Math.floor((Math.random() * 40) + 1);
+//document.getElementById("demo").innerHTML = x;
+var cards = [];
+
+for (var i = 1; i <= 40; i++) {
+   cards.push(i);
+}
+
+alert(cards);
