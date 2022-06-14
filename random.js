c289cc1e675054ff2d580b7915bcc4971901369f
Ritterstahl <72339963+Ritterstahl@users.noreply.github.com>
2022-06-14 16:52:48 +0100

    Update random.js

--- random.js
+++ random.js
@@ -1,9 +1,14 @@
 //var x = Math.floor((Math.random() * 40) + 1);
 //document.getElementById("demo").innerHTML = x;
+
 var cards = [];
 
 for (var i = 1; i <= 40; i++) {
    cards.push(i);
 }
 
-
+var myIndex = cards.indexOf(5);
+if (myIndex !== -1) {
+    cards.splice(myIndex, 1);
+}
+console.log(cards);
