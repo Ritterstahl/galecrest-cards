ab501e93f88fdf0fdc2ceb946176dac82a163414
Ritterstahl <72339963+Ritterstahl@users.noreply.github.com>
2022-06-15 17:21:41 +0100

    Update random.js

--- random.js
+++ random.js
@@ -10,15 +10,32 @@ for (var i = 1; i <= numCards; i++) {
    cards.push(i);
 }
 
+
+
+
+//const randomItem = arr => arr.splice((Math.random() * arr.length) | 0, 1);
+
+//const arr = [1, 3, 5, 7, 9];
+//randomItem(arr);    // 7
+                      // arr = [1, 3, 5, 9]
+
+
+const randomCard = cards => cards.splice((Math.random() * cards.length) | 0, 1);
+
+
+
 function chooseCard() {
-  var c = Math.floor((Math.random() * cards.length) + 1);
-  var i = cards.indexOf(c);
-  if (i !== -1) {
-    cards.splice(i, 1);
-    numCards --;
-  } else {
-    chooseCard(); 
-  }
+  //var c = Math.floor((Math.random() * cards.length) + 1);
+  //var i = cards.indexOf(c);
+   
+  var c = randomCard(cards);
+   
+  //if (i !== -1) {
+    //cards.splice(i, 1);
+    //numCards --;
+  //} else {
+    //chooseCard(); 
+  //}
   $('#card'+(card)).append(c);
   console.log(c);
   console.log(cards); 
