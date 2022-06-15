87acd30d99a729ed20bd13fd94b45204125f4e50
Ritterstahl <72339963+Ritterstahl@users.noreply.github.com>
2022-06-15 16:59:30 +0100

    Update random.js

--- random.js
+++ random.js
@@ -4,18 +4,21 @@
 var cards = [];
 var voyage = 1;
 var card = 0;
+var numCards = 40;
 
 for (var i = 1; i <= 40; i++) {
    cards.push(i);
 }
 
 function chooseCard() {
-  var c = Math.floor((Math.random() * 40) + 1);
+  var c = Math.floor((Math.random() * numCards) + 1);
   var i = cards.indexOf(c);
   if (i !== -1) {
     cards.splice(i, 1);
+    numCards --;
   }
   $('#day'+(card+1)).append(c);
+  console.log(c);
   console.log(cards); 
 }
 
